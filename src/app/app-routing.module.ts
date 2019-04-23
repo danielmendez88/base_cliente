a2c426e96eaeae2e4a0f473fcc8cdc1e3b2e3580
daniel mendez cruz <danielmendez.88@live.com>
2019-04-23 13:54:53 -0500

    modificaciones y adiciones de la rama alex a la principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-// rutas
+
 import { AgendaComponent } from './agenda/agenda.component';
+import { ComisionesComponent } from './comisiones/comisiones.component';
 import { DetalleAgendaComponent } from './detalle-agenda/detalle-agenda.component';
-import { Page404Component } from './page404/page404.component';
 import { NuevoUsuarioComponent } from './nuevo-usuario/nuevo-usuario.component';
+<<<<<<< HEAD
 // documento
 import { DocumentoComisionComponent } from './documento-comision/documento-comision.component';
 
@@ -15,6 +16,16 @@ const routes: Routes = [
   { path: 'usuario/:id', component: DetalleAgendaComponent},
   { path: 'documento/comision', component: DocumentoComisionComponent}, // documento comision ruta
   { path: '**', component: Page404Component},
+=======
+import { Page404Component } from './page404/page404.component';
+
+const routes: Routes = [
+  { path: '', component: AgendaComponent},
+  { path: 'comisiones/lista', component: AgendaComponent },
+  { path: 'comisiones/nuevo', component: ComisionesComponent},
+  { path: 'comisiones/:id', component: DetalleAgendaComponent},
+  { path: '**', component: Page404Component}
+>>>>>>> 16878b4c096ab768e306f6ddc6ec9e867cdd0f04
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule  } from '@angular/forms';
+import { MAT_STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-// animaciones
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-// material
-import { MaterialModule } from './materia.modeule';
-
-// preparar y configurar el cliente http para leer el servicio
-import { HttpClientModule } from '@angular/common/http';
-import { DetalleAgendaComponent, DialogOAgendaOverview } from './detalle-agenda/detalle-agenda.component';
-import { Page404Component } from './page404/page404.component';
+import { MaterialModule } from './Material/materia.module';
 import { AgendaComponent } from './agenda/agenda.component';
-
-// servicios
+import { ComisionesComponent } from './comisiones/comisiones.component';
+import { DetalleAgendaComponent, DialogOAgendaOverview } from './detalle-agenda/detalle-agenda.component';
+import { EditAgendaComponent } from './edit-agenda/edit-agenda.component';
+import { NuevoUsuarioComponent } from './nuevo-usuario/nuevo-usuario.component';
+import { Page404Component } from './page404/page404.component';
 import { DataApiService } from './services/data-api.service';
+<<<<<<< HEAD
 import { NuevoUsuarioComponent } from './nuevo-usuario/nuevo-usuario.component';
 import { EditAgendaComponent } from './edit-agenda/edit-agenda.component';
 // formularios
 import { ReactiveFormsModule, FormsModule  } from '@angular/forms';
 import { DocumentoComisionComponent } from './documento-comision/documento-comision.component';
+=======
+>>>>>>> 16878b4c096ab768e306f6ddc6ec9e867cdd0f04
 
 @NgModule({
   declarations: [
@@ -32,7 +35,11 @@ import { DocumentoComisionComponent } from './documento-comision/documento-comis
     Page404Component,
     NuevoUsuarioComponent,
     EditAgendaComponent,
+<<<<<<< HEAD
     DocumentoComisionComponent,
+=======
+    ComisionesComponent,
+>>>>>>> 16878b4c096ab768e306f6ddc6ec9e867cdd0f04
   ],
   imports: [
     BrowserModule,
@@ -41,9 +48,17 @@ import { DocumentoComisionComponent } from './documento-comision/documento-comis
     MaterialModule,
     HttpClientModule,
     ReactiveFormsModule,
-    FormsModule ,
+    FormsModule,
+    FlexLayoutModule
   ],
-  providers: [DataApiService],
+  providers: [
+    DataApiService,
+    {
+      provide: MAT_STEPPER_GLOBAL_OPTIONS,
+      useValue: { showError: true }
+    }
+  ],
+  
   bootstrap: [AppComponent],
   entryComponents: [DialogOAgendaOverview]
 })
