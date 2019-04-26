2e77bf598f921dca94f299af146408f93220e7c4
daniel mendez cruz <danielmendez.88@live.com>
2019-04-26 10:37:03 -0500

    modificaciones de las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-//import inicio
-import { WelcomeComponent } from "./welcome/welcome.component";
-//imports para login
+// import inicio
+import { WelcomeComponent } from './welcome/welcome.component';
+// imports para login
 import { LoginComponent } from './auth/login/login.component';
 import { AuthGuard } from './auth/auth.guard';
 import { GuessGuard } from './auth/guess.guard';
@@ -17,21 +17,14 @@ import { Page404Component } from './page404/page404.component';
 
 
 const routes: Routes = [
-<<<<<<< HEAD
-  { path: '', component: AgendaComponent},
-  { path: 'comisiones/list', component: ComisionesListaComponent },
-  { path: 'comisiones/nuevo', component: ComisionesComponent},
-  { path: 'comisiones/:id', component: DetalleAgendaComponent},
-=======
-  //{ path: '', component: WelcomeComponent },//pantalla de inicio
-  { path: '', component: LoginComponent ,canActivate: [GuessGuard]},//login 
+  // { path: '', component: WelcomeComponent },//pantalla de inicio
+  { path: '', component: LoginComponent , canActivate: [GuessGuard]}, // login
   { path: 'agenda', component: AgendaComponent, canActivate: [GuessGuard]},
   { path: 'comisiones/list', component: ComisionesListaComponent , canActivate: [GuessGuard]}, // comisiones lista
   { path: 'comisiones/nuevo', component: ComisionesComponent, canActivate: [GuessGuard]},
   { path: 'comisiones/:id', component: DetalleAgendaComponent, canActivate: [GuessGuard]},
->>>>>>> ac994fae97c746056d483ae336d504e208a5aae0
   { path: '**', component: Page404Component},
- 
+
 ];
 
 @NgModule({
